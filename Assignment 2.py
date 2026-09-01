ea06b3979e8d2dfb2cdb561f94711766de2559e1
athbauskar-byte <ath.bauskar@gmail.com>
2026-09-01 17:49:11 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,12 @@
+a=int(input("your number is:"))
+b=int(input("your number is:"))
+c=int(input("your number is:"))
+
+if a>b and a>c:
+    print("a is the largest number")
+    
+elif b>a and b>c:
+    print("b is the largest number")
+    
+else:
+    print("c is the largest number")
